8d78afa491c4ace20bfe3680106323ec62f4440b
Alireza Azimi <alirezaazimi@Alirezas-MacBook-Pro.local>
2023-05-10 22:56:53 -0600

    initial commit

---
+++ Buzz/App.tsx
@@ -0,0 +1,97 @@
+/**
+ * Buzz Password Manager
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type { PropsWithChildren } from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import customTheme from './buzzTheme';
+import { FAB, Provider as PaperProvider, Appbar } from 'react-native-paper';
+
+// type SectionProps = PropsWithChildren<{
+//   title: string;
+// }>;
+
+// function Section({children, title}: SectionProps): JSX.Element {
+//   const isDarkMode = useColorScheme() === 'dark';
+//   return (
+//     <View style={styles.sectionContainer}>
+//       <Text
+//         style={[
+//           styles.sectionTitle,
+//           {
+//             color: isDarkMode ? Colors.white : Colors.black,
+//           },
+//         ]}>
+//         {title}
+//       </Text>
+//       <Text
+//         style={[
+//           styles.sectionDescription,
+//           {
+//             color: isDarkMode ? Colors.light : Colors.dark,
+//           },
+//         ]}>
+//         {children}
+//       </Text>
+//     </View>
+//   );
+// }
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+
+  return (
+    <PaperProvider theme={customTheme}>
+      <SafeAreaView style={styles.container}>
+        <Appbar >
+          <Appbar.Content title="Buzz Password Manager" />
+        </Appbar>
+        <FAB
+          icon="plus"
+          style={styles.fab}
+          onPress={() => console.log('Pressed')}
+        />
+      </SafeAreaView>
+    </PaperProvider>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  fab: {
+    position: 'absolute',
+    margin: 16,
+    right: 16,
+    bottom: 16,
+  },
+});
+
+export default App;
---
+++ Buzz/buzzTheme.tsx
@@ -0,0 +1,49 @@
+import { MD3DarkTheme as DefaultTheme } from "react-native-paper";
+const themeObject = {
+    ...DefaultTheme,
+
+        "colors": {
+          "primary": "rgb(233, 195, 72)",
+          "onPrimary": "rgb(60, 47, 0)",
+          "primaryContainer": "rgb(87, 69, 0)",
+          "onPrimaryContainer": "rgb(255, 224, 136)",
+          "secondary": "rgb(254, 186, 75)",
+          "onSecondary": "rgb(68, 43, 0)",
+          "secondaryContainer": "rgb(97, 64, 0)",
+          "onSecondaryContainer": "rgb(255, 221, 177)",
+          "tertiary": "rgb(200, 191, 255)",
+          "onTertiary": "rgb(48, 33, 117)",
+          "tertiaryContainer": "rgb(70, 58, 141)",
+          "onTertiaryContainer": "rgb(229, 222, 255)",
+          "error": "rgb(255, 180, 171)",
+          "onError": "rgb(105, 0, 5)",
+          "errorContainer": "rgb(147, 0, 10)",
+          "onErrorContainer": "rgb(255, 180, 171)",
+          "background": "rgb(30, 27, 22)",
+          "onBackground": "rgb(232, 226, 217)",
+          "surface": "rgb(30, 27, 22)",
+          "onSurface": "rgb(232, 226, 217)",
+          "surfaceVariant": "rgb(76, 70, 57)",
+          "onSurfaceVariant": "rgb(207, 198, 180)",
+          "outline": "rgb(152, 144, 128)",
+          "outlineVariant": "rgb(76, 70, 57)",
+          "shadow": "rgb(0, 0, 0)",
+          "scrim": "rgb(0, 0, 0)",
+          "inverseSurface": "rgb(232, 226, 217)",
+          "inverseOnSurface": "rgb(51, 48, 42)",
+          "inversePrimary": "rgb(115, 92, 0)",
+          "elevation": {
+            "level0": "transparent",
+            "level1": "rgb(40, 35, 25)",
+            "level2": "rgb(46, 40, 26)",
+            "level3": "rgb(52, 46, 28)",
+            "level4": "rgb(54, 47, 28)",
+            "level5": "rgb(58, 51, 29)"
+          },
+          "surfaceDisabled": "rgba(232, 226, 217, 0.12)",
+          "onSurfaceDisabled": "rgba(232, 226, 217, 0.38)",
+          "backdrop": "rgba(53, 48, 36, 0.4)"
+        }
+      
+}
+export default themeObject
